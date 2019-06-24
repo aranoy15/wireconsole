61d1dadb6961512d4045ad669e424555c8c729d2
Anton Fedoseev <aranoy15@gmail.com>
2019-06-24 15:45:48 +0300

    drawing test

--- logic/wireconsole/widgets/src/drawwidget.py
+++ logic/wireconsole/widgets/src/drawwidget.py
@@ -6,11 +6,13 @@ class DrawWidget(QtWidgets.QWidget, Ui_DrawWidget):
         super().__init__()
         self.setupUi(self)
 
-        self.scene = QtWidgets.QGraphicsScene()
+        self.scene = QtWidgets.QGraphicsScene(self)
         self.view = QtWidgets.QGraphicsView(self.scene)
 
         self.horizontalLayout.addWidget(self.view)
 
         pen = QtGui.QPen(QtCore.Qt.red)
         rect = QtCore.QRectF(100.0, 100.0, 10.0, 10.0)
+
         self.scene.addRect(rect, pen)
+        self.scene.addLine(100.0, -200.0, 100.0, 100.0, QtGui.QPen(QtCore.Qt.blue))
